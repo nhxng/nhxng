df48ca282900ef5cb294e6881d25c5a6868d2d4f
Nhung Nguyen <pn.nguyen@outlook.com>
2017-08-14 10:34:15 +0200

    rails new

---
+++ reboot/christmas-list/christmas_list.rb
@@ -0,0 +1,13 @@
+# (list / add / delete / mark )
+# 1. Display wishlist
+# 2. Add a gift to wishlist
+# 3. Mark a gift
+# 4. Delete a gift
+
+# Actions:
+# 1. Display wishlist
+
+# 2. Add gift
+# ask user for gift
+# store in list
+#
